c9b33ff540aeb4b6b02f8aa274af1b311e61f698
AnmolMishra29 <68518640+AnmolMishra29@users.noreply.github.com>
2023-02-03 04:25:54 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import Appbar from './components/frontpage/Appbar';
 import EthnicDashboard from './components/dashboard/EthnicDashboard';
+import ManufacturingDashboard from './components/dashboard/ManufacturingDashboard';
+import MarketingDashboard from './components/dashboard/MarketingDashboard';
+import ProductDashboard from './components/dashboard/ProductDashboard';
+import MallDetails from './components/detailMall/MallDetails';
+import EngineeringMode from './components/detailMall/EngineeringMode';
+
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-       {/* <Appbar/> */}
-       <EthnicDashboard/>
-    </>
+    <BrowserRouter>
+        <Routes>
+           <Route path='/' element= {<Appbar/>}/>
+           <Route path='ethnic' element= {<EthnicDashboard/>}/>
+           <Route path='marketing' element= {<MarketingDashboard/>}/>
+           <Route path='manufacturing' element= {<ManufacturingDashboard/>}/>
+           <Route path='product' element= {<ProductDashboard/>}/>
+           <Route path='malldetails' element= {<MallDetails/>}/>
+           <Route path='engineering' element= {<EngineeringMode/>}/>
+        </Routes>
+       
+    </BrowserRouter>
   );
 }
 
