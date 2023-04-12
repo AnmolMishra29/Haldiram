452eb90cef6fbafbfbc39b6ca56b6612540af17c
Anmol Mishra <68518640+AnmolMishra29@users.noreply.github.com>
2023-04-12 11:19:35 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Appbar from "./components/frontpage/Appbar";
 import EthnicDashboard from "./components/dashboard/EthnicDashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FourZeroFour from "./components/FourZeroFour";
 import MainDashboard from "./components/dashboard/MainDashboard";
 import MallDetails from "./components/StandartMode/MallDetails";
 import EngineeringMode from "./components/StandartMode/EngineeringMode";
@@ -10,6 +9,11 @@ import UserRegistration from "./components/registration/UserRegistration";
 import DeviceRegistration from "./components/registration/DeviceRegistration";
 import CounterRegistration from "./components/registration/CounterRegistration";
 import StoreRegistration from "./components/registration/StoreRegistration";
+import MainLogin from "./components/frontpage/MainLogin";
+import UserTable from "./components/table/UserTable";
+import ProSidebaar from "./components/registration/ProSidebaar";
+import { CssBaseline } from "@mui/material";
+import Newlist from "./components/registration/Newlist";
 
 function App() {
   return (
@@ -41,11 +45,57 @@ function App() {
           <Route exact path="/MainDashboard" element={<MainDashboard />} />
           <Route exact path="/mallDetails" element={<MallDetails />} />
           <Route exact path="/engineeringmode" element={<EngineeringMode />} />
-
-          <Route path="*" element={<FourZeroFour />} />
+          <Route exact path="/registration" element={<ProSidebaar />} />
+          <Route
+            exact
+            path="registration/userregistration"
+            element={<UserRegistration />}
+          />
+          <Route
+            exact
+            path="/registration/storeregistration"
+            element={<StoreRegistration />}
+          />
+          <Route
+            exact
+            path="/registration/deviceregistration"
+            element={<DeviceRegistration />}
+          />
+          <Route
+            exact
+            path="/registration/counterregistration"
+            element={<CounterRegistration />}
+          />
+          <Route exact path="/registration/allusers" element={<Newlist />} />
         </Routes>
-      </BrowserRouter> */}
-      <DeviceRegistration/>
+      </BrowserRouter> 
+      */}
+      <BrowserRouter>
+        <ProSidebaar />
+        <Routes>
+          <Route
+            exact
+            path="/userregistration"
+            element={<UserRegistration />}
+          />
+          <Route
+            exact
+            path="/storeregistration"
+            element={<StoreRegistration />}
+          />
+          <Route
+            exact
+            path="/deviceregistration"
+            element={<DeviceRegistration />}
+          />
+          <Route
+            exact
+            path="/counterregistration"
+            element={<CounterRegistration />}
+          />
+          <Route exact path="/allusers" element={<Newlist />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
